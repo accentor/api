e96089e4b417dbe8b2bdb1797680cad45935cc37
Charlotte Van Petegem <charlotte@vanpetegem.me>
2019-04-12 10:16:31 +0200

    Also seed an admin user when creating the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
+user = User.create(name: 'admin', password: 'admin', permission: :admin)
+
 if Rails.env.development?
-  user = User.create(name: 'charlotte', password: 'password', permission: :admin)
   token = AuthToken.create(user: user, user_agent: 'Rails')
   token.update(hashed_secret: BCrypt::Password.create('secret', cost: 1), device_id: 'device-id')
 end
