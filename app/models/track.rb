b3f68c14eff14845fdae30b80d58d27269c39c2e
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-03-04 20:46:41 +0100

    Search fixes

--- app/models/track.rb
+++ app/models/track.rb
@@ -29,7 +29,7 @@ class Track < ApplicationRecord
 
   before_save :normalize_artist_order
 
-  scope :by_filter, ->(filter) {where("normalized_title LIKE ?", "%#{Track.normalize(filter)}%")}
+  scope :by_filter, ->(filter) {where('"tracks"."normalized_title" LIKE ?', "%#{Track.normalize(filter)}%")}
   scope :by_artist, ->(artist) {joins(:artists).where(artists: {id: artist})}
   scope :by_album, ->(album) {where(album: album)}
   scope :by_genre, ->(genre) {joins(:genres).where(genres: {id: genre})}
