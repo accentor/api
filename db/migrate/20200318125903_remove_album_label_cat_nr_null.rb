00be6e0e231a441d5183140afd8f9d96020e84d2
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-03-19 17:25:45 +0100

    Fix syntax error in migration

--- db/migrate/20200318125903_remove_album_label_cat_nr_null.rb
+++ db/migrate/20200318125903_remove_album_label_cat_nr_null.rb
@@ -3,6 +3,5 @@ class RemoveAlbumLabelCatNrNull < ActiveRecord::Migration[6.0]
     change_column_null :album_labels, :catalogue_number, true
 
     AlbumLabel.where(catalogue_number: "none").update_all(catalogue_number: nil)
-    end
   end
 end
