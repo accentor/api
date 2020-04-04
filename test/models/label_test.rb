0fea5106f7f0fa2977e80c2837cc19ebb3f3dd1c
Robbe Van Petegem <robbe@vanpetegem.me>
2020-04-04 17:37:51 +0200

    Test labels merge

--- test/models/label_test.rb
+++ test/models/label_test.rb
@@ -16,4 +16,40 @@ class LabelTest < ActiveSupport::TestCase
     assert_not label.normalized_name.nil?
     assert_equal 'iouaaa', label.normalized_name
   end
+
+  test 'should be able to merge label' do
+    label1 = create(:label)
+    label2 = create(:label)
+
+    assert_difference('Label.count', -1) do
+      label2.merge(label1)
+    end
+  end
+
+  test 'should be able to merge labels if one belongs to album' do
+    label1 = create(:label)
+    label2 = create(:label)
+    album = create(:album)
+    al1 = create(:album_label, album: album, label: label1)
+
+    assert_difference('Label.count', -1) do
+      label2.merge(label1)
+    end
+    assert_not album.reload.labels.include?(label1)
+    assert album.reload.labels.include?(label2)
+  end
+
+  test 'should be able to merge labels if track belongs to both' do
+    label1 = create(:label)
+    label2 = create(:label)
+    album = create(:album)
+    al1 = create(:album_label, album: album, label: label1)
+    al2 = create(:album_label, album: album, label: label2)
+
+    assert_difference('Label.count', -1) do
+      label2.merge(label1)
+    end
+    assert_not album.reload.labels.include?(label1)
+    assert album.reload.labels.include?(label2)
+  end
 end
