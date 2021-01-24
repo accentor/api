41dbcfc2d26c8e9716e6e9895d09b6cdee8f7d12
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-01-24 17:54:30 +0100

    Allow loggin delayed job to stdout

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,7 +2,11 @@ Delayed::Worker.destroy_failed_jobs = true
 Delayed::Worker.max_attempts = 1
 Delayed::Worker.max_run_time = 1.day
 Delayed::Worker.raise_signal_exceptions = :term
-Delayed::Worker.logger = Logger.new(Rails.root.join("log/delayed_job_#{Rails.env}.log"))
+if ENV['RAILS_LOG_TO_STDOUT'].present?
+  Delayed::Worker.logger = Logger.new($stdout)
+else
+  Delayed::Worker.logger = Logger.new(Rails.root.join("log/delayed_job_#{Rails.env}.log"))
+end
 Delayed::Worker.queue_attributes = {
   transcodes: { priority: 5 },
   rescans: { priority: 10 },
