f077fdd03b06ad484cb0b5e73bc4530a5aa556e6
Robbe Van Petegem <robbe@vanpetegem.me>
2021-02-21 15:00:54 +0100

    Rework tests for recalculation of ContentLength

--- test/models/content_length_test.rb
+++ test/models/content_length_test.rb
@@ -26,21 +26,60 @@ class ContentLengthTest < ActiveSupport::TestCase
   end
 
   test 'should remain if ffmpeg version matches' do
-    @content_length.check_ffmpeg_version
-    @content_length.reload
-    assert_not_nil @content_length.id
+    assert_difference('ContentLength.count', 0) do
+      @content_length.check_ffmpeg_version
+    end
   end
 
-  test 'should destroy if ffmpeg version does not match' do
+  test 'should not create new ContentLength if audio is longer than config and track is older than config' do
+    @track = create(:track, audio_file: @audio_file)
+
     # rubocop:disable Rails/SkipsModelValidations
-    # We want to avoid our own before_save callback to manually set a wrong version
+    # We want to avoid our own before_save callbacks to manually set a wrong version and audio_file length
+    @audio_file.update_column(:length, 1)
+    @audio_file.track.update_column(:created_at, 1.year.ago)
     @content_length.update_column(:ffmpeg_version, Faker::App.semantic_version)
     # rubocop:enable Rails/SkipsModelValidations
-    assert_not_equal Rails.application.config.FFMPEG_VERSION, @content_length.ffmpeg_version
-    @content_length.check_ffmpeg_version
 
-    assert_raises(ActiveRecord::RecordNotFound) do
-      @content_length.reload
+    assert_difference('ContentLength.count', -1) do
+      @content_length.check_ffmpeg_version
     end
+  end
+
+  test 'should create new ContentLength if audio is longer than config' do
+    io = StringIO.new File.read(Rails.root.join('test/files/base.flac'))
+    AudioFile.any_instance.stubs(:convert).returns(io)
+    @track = create(:track, audio_file: @audio_file)
+
+    # rubocop:disable Rails/SkipsModelValidations
+    # We want to avoid our own before_save callbacks to manually set a wrong version and audio_file length
+    @content_length.update_column(:ffmpeg_version, Faker::App.semantic_version)
+    @audio_file.update_column(:length, 1000)
+    @audio_file.track.update_column(:created_at, 1.year.ago)
+    # rubocop:enable Rails/SkipsModelValidations
+
+    assert_difference('ContentLength.count', 0) do
+      @content_length.check_ffmpeg_version
+    end
+  end
+
+  test 'should create new ContentLength if track is newer than config' do
+    io = StringIO.new File.read(Rails.root.join('test/files/base.flac'))
+    AudioFile.any_instance.stubs(:convert).returns(io)
+    @track = create(:track, audio_file: @audio_file)
+
+    # rubocop:disable Rails/SkipsModelValidations
+    # We want to avoid our own before_save callbacks to manually set a wrong version and audio_file length
+    @content_length.update_column(:ffmpeg_version, Faker::App.semantic_version)
+    @audio_file.update_column(:length, 1)
+    @audio_file.track.update_column(:created_at, 1.day.ago)
+    # rubocop:enable Rails/SkipsModelValidations
+
+    assert_difference('ContentLength.count', 0) do
+      @content_length.check_ffmpeg_version
+    end
+
+    @audio_file.reload
+    assert_equal 1, @audio_file.content_lengths.length
   end
 end
