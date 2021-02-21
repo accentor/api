4733830976d7ad8ad16de10fe15ffa6b01c3ef6a
Robbe Van Petegem <robbe@vanpetegem.me>
2021-02-21 11:45:43 +0100

    Set FFMPEG_VERSION on startup

---
+++ config/initializers/ffmpeg.rb
@@ -0,0 +1,10 @@
+require 'open3'
+
+if Rails.env.test?
+  # We don't require test setups to have FFMPEG installed, so can jsut manually set a version
+  Rails.application.config.FFMPEG_VERSION = '0.0.1'
+else
+  stdin, stdout, = Open3.popen2('ffmpeg -version')
+  stdin.close
+  Rails.application.config.FFMPEG_VERSION = stdout.gets.match(/ffmpeg version (\d\.\d\.\d)/)[1]
+end
