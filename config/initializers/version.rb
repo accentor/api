ebf9355a32ce04243a2d7b0bea816b5ae679a466
Robbe Van Petegem <robbe@vanpetegem.me>
2021-09-08 21:03:32 +0200

    Set version inside application and add to headers

---
+++ config/initializers/version.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Accentor
+  class Application
+    module Version
+      MAJOR = 0
+      MINOR = 13
+      PATCH = 0
+
+      STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+    end
+    VERSION = Version::STRING
+
+    # Set version header for every request
+    config.action_dispatch.default_headers.merge!('X-API-Version' => VERSION)
+  end
+end
