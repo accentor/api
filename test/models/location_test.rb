3f2e43334394801631ec31ee64c176469f6610d4
Robbe Van Petegem <robbe@vanpetegem.me>
2021-09-19 13:35:39 +0200

    Verify new Location is not parent or subdir of existing path

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -9,4 +9,26 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
+  setup do
+    create(:location, path: '/var/parent')
+  end
+
+  test 'location cant be subdirectory of other location' do
+    child = build(:location, path: '/var/parent/music')
+    assert_not child.valid?
+    assert_not_empty child.errors[:path]
+  end
+
+  test 'location cant be parent of other location' do
+    parent = build(:location, path: '/var')
+    assert_not parent.valid?
+    assert_not_empty parent.errors[:path]
+  end
+
+  test 'location should match parent/subdir regardless of case' do
+    parent = build(:location, path: '/Var')
+    assert_not parent.valid?
+    child = build(:location, path: '/Var/parent/Music')
+    assert_not child.valid?
+  end
 end
