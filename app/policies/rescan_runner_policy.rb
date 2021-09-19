a7cde849a248daa0da25dbe6a81252df9d6e61b3
Robbe Van Petegem <robbe@vanpetegem.me>
2021-09-19 16:20:41 +0200

    Fix policy

--- app/policies/rescan_runner_policy.rb
+++ app/policies/rescan_runner_policy.rb
@@ -1,9 +1,23 @@
 class RescanRunnerPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all if user&.moderator?
+    end
+  end
+
+  def index?
+    user&.moderator?
+  end
+
   def show?
     user&.moderator?
   end
 
   def start?
+    user&.moderator?
+  end
+
+  def start_all?
     user&.moderator?
   end
 end
