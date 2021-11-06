31285ca81b09c3281d6fa48c7dae09de7c37ac04
robbevp <robbe@vanpetegem.me>
2021-11-06 09:59:07 +0100

    Verify new Location is not parent or subdir of existing path (#254)
    * Verify new Location is not parent or subdir of existing path
    
    * Remove case sensitivity

--- app/models/location.rb
+++ app/models/location.rb
@@ -10,4 +10,16 @@ class Location < ApplicationRecord
   has_many :audio_files, dependent: :destroy
 
   validates :path, presence: true, uniqueness: true
+  validate :cant_be_subdir_of_other_location
+  validate :cant_be_parent_of_other_location
+
+  private
+
+  def cant_be_subdir_of_other_location
+    errors.add(:path, 'path-is-subdirectoy') unless Location.where('path LIKE ?', "#{path}%").empty?
+  end
+
+  def cant_be_parent_of_other_location
+    errors.add(:path, 'path-is-parent') unless Location.where("? LIKE path || '%'", path).empty?
+  end
 end
