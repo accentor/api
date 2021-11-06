51f5f1eebb1110f42cc5777da64f680fa64c2c96
robbevp <robbe@vanpetegem.me>
2021-11-06 13:09:31 +0100

    Make sure we allow two sibling dirs (#268)

--- app/models/location.rb
+++ app/models/location.rb
@@ -10,16 +10,18 @@ class Location < ApplicationRecord
   has_many :audio_files, dependent: :destroy
 
   validates :path, presence: true, uniqueness: true
-  validate :cant_be_subdir_of_other_location
-  validate :cant_be_parent_of_other_location
+  validate :cant_be_parent_or_subdir_of_other_location
+
+  def expanded_path
+    Pathname.new(path).expand_path
+  end
 
   private
 
-  def cant_be_subdir_of_other_location
-    errors.add(:path, 'path-is-subdirectoy') unless Location.where('path LIKE ?', "#{path}%").empty?
-  end
-
-  def cant_be_parent_of_other_location
-    errors.add(:path, 'path-is-parent') unless Location.where("? LIKE path || '%'", path).empty?
+  def cant_be_parent_or_subdir_of_other_location
+    Location.find_each do |l|
+      errors.add(:path, 'path-is-subdirectoy') if expanded_path.fnmatch?(File.join(l.expanded_path, '**'))
+      errors.add(:path, 'path-is-parent') if l.expanded_path.fnmatch?(File.join(expanded_path, '**'))
+    end
   end
 end
