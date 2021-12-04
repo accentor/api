0556f64031e910bb35bf19624d28d2e6ac656fb4
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-12-04 15:32:05 +0100

    Fix migration for rescan runner per location

--- db/migrate/20210905090539_add_location_to_rescan_runner.rb
+++ db/migrate/20210905090539_add_location_to_rescan_runner.rb
@@ -1,6 +1,6 @@
 class AddLocationToRescanRunner < ActiveRecord::Migration[6.1]
   def change
-    RescanRunner.destroy_all
+    ActiveRecord::Base.connection.execute("TRUNCATE rescan_runners")
 
     add_reference :rescan_runners, :location, null: false, foreign_key: true
 
