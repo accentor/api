e050e438aede7ab859ff02b820823407d95f5ec1
Robbe Van Petegem <robbe@vanpetegem.me>
2022-04-02 10:21:44 +0200

    Allow classes to normalize blank values

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -7,4 +7,17 @@ class ApplicationRecord < ActiveRecord::Base
   def self.normalize(str)
     str.unicode_normalize(:nfkd).gsub(/[\u0300-\u036f]/, '').downcase
   end
+
+  def self.normalize_blank_values(*cols)
+    cols.each do |col|
+      col_changed = :"#{col}_changed?"
+      method_name = :"normalize_blank_#{col}_value"
+
+      set_callback :validation, :before, method_name, if: col_changed
+
+      define_method method_name do
+        self[col].present? || self[col] = nil
+      end
+    end
+  end
 end
