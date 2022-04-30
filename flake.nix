efe9bae44bb56387f41b9080694a1a6358016750
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-04-30 14:04:31 +0200

    Clean up flake.nix a little

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Accentor API";
-    
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -13,16 +13,10 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    let
-      inherit (flake-utils.lib) eachDefaultSystem;
-    in
-    eachDefaultSystem (system:
+  outputs = { self, nixpkgs, devshell, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ inputs.devshell.overlay ];
-        };
+        pkgs = import nixpkgs { inherit system; overlays = [ devshell.overlay ]; };
         gems = pkgs.bundlerEnv rec {
           name = "accentor-api-env";
           ruby = pkgs.ruby_3_1;
@@ -33,77 +27,84 @@
         };
       in
       {
-        defaultPackage = pkgs.stdenv.mkDerivation rec {
-          pname = "accentor-api";
-          version = "0.17.2";
+        packages = rec {
+          default = accentor-api;
+          accentor-api = pkgs.stdenv.mkDerivation rec {
+            pname = "accentor-api";
+            version = "0.17.2";
 
-          src = pkgs.lib.cleanSourceWith { filter = name: type: !(builtins.elem name [ ".github" "flake.lock" "flake.nix" ]); src = ./.; name = "source"; };
+            src = pkgs.lib.cleanSourceWith { filter = name: type: !(builtins.elem name [ ".github" "flake.lock" "flake.nix" ]); src = ./.; name = "source"; };
 
-          installPhase = ''
-            mkdir $out
-            cp -r * $out
-          '';
+            installPhase = ''
+              mkdir $out
+              cp -r * $out
+            '';
 
-          passthru.env = gems;
+            passthru.env = gems;
+          };
         };
 
-        devShell = pkgs.devshell.mkShell {
-          name = "Accentor API";
-          packages = [
-            gems
-            (pkgs.lowPrio gems.wrappedRuby)
-            pkgs.bundix
-            pkgs.ffmpeg
-            pkgs.postgresql_14
-          ];
-          env = [
-            {
-              name = "PGDATA";
-              eval = "$PWD/tmp/postgres";
-            }
-            {
-              name = "DATABASE_HOST";
-              eval = "$PGDATA";
-            }
-          ];
-          commands = [
-            {
-              name = "pg:setup";
-              category = "database";
-              help = "Setup postgres in project folder";
-              command = ''
-                initdb --encoding=UTF8 --no-locale --no-instructions -U postgres
-                echo "listen_addresses = ${"'"}${"'"}" >> $PGDATA/postgresql.conf
-                echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf
-                echo "CREATE USER accentor WITH PASSWORD 'accentor' CREATEDB;" | postgres --single -E postgres
-              '';
-            }
-            {
-              name = "pg:start";
-              category = "database";
-              help = "Start postgres instance";
-              command = ''
-                [ ! -d $PGDATA ] && setup-db
-                pg_ctl -D $PGDATA -U postgres start -l log/postgres.log
-              '';
-            }
-            {
-              name = "pg:stop";
-              category = "database";
-              help = "Stop postgres instance";
-              command = ''
-                pg_ctl -D $PGDATA -U postgres stop
-              '';
-            }
-            {
-              name = "pg:console";
-              category = "database";
-              help = "Open database console";
-              command = ''
-                psql --host $PGDATA -U postgres 
-              '';
-            }
-          ];
+        devShells = rec {
+          default = accentor-api;
+          accentor-api = pkgs.devshell.mkShell {
+            name = "Accentor API";
+            packages = [
+              gems
+              (pkgs.lowPrio gems.wrappedRuby)
+              pkgs.bundix
+              pkgs.ffmpeg
+              pkgs.nixpkgs-fmt
+              pkgs.postgresql_14
+            ];
+            env = [
+              {
+                name = "PGDATA";
+                eval = "$PWD/tmp/postgres";
+              }
+              {
+                name = "DATABASE_HOST";
+                eval = "$PGDATA";
+              }
+            ];
+            commands = [
+              {
+                name = "pg:setup";
+                category = "database";
+                help = "Setup postgres in project folder";
+                command = ''
+                  initdb --encoding=UTF8 --no-locale --no-instructions -U postgres
+                  echo "listen_addresses = ${"'"}${"'"}" >> $PGDATA/postgresql.conf
+                  echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf
+                  echo "CREATE USER accentor WITH PASSWORD 'accentor' CREATEDB;" | postgres --single -E postgres
+                '';
+              }
+              {
+                name = "pg:start";
+                category = "database";
+                help = "Start postgres instance";
+                command = ''
+                  [ ! -d $PGDATA ] && setup-db
+                  pg_ctl -D $PGDATA -U postgres start -l log/postgres.log
+                '';
+              }
+              {
+                name = "pg:stop";
+                category = "database";
+                help = "Stop postgres instance";
+                command = ''
+                  pg_ctl -D $PGDATA -U postgres stop
+                '';
+              }
+              {
+                name = "pg:console";
+                category = "database";
+                help = "Open database console";
+                command = ''
+                  psql --host $PGDATA -U postgres
+                '';
+              }
+            ];
+          };
         };
       }
     );
