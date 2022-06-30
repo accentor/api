0735a9ae65f0b86ae7d81965f4278e4e91bc1c1f
Robbe Van Petegem <robbe@vanpetegem.me>
2022-06-30 19:28:26 +0200

    Add endpoint to get play stats (#340)

--- test/controllers/plays_controller_test.rb
+++ test/controllers/plays_controller_test.rb
@@ -41,4 +41,51 @@ class PlaysControllerTest < ActionDispatch::IntegrationTest
 
     assert_response 422
   end
+
+  test 'should get stats and  not return play stats for other users' do
+    create(:play, track: @track, user: create(:user))
+    get stats_plays_url
+    assert_response :success
+    body = JSON.parse response.body
+    assert_empty body
+  end
+
+  test 'should get stats and  return play stats for users' do
+    create(:play, track: @track, user: @user, played_at: DateTime.new(2022, 0o1, 0o2, 0o3, 0o4, 0o5))
+    get stats_plays_url
+    assert_response :success
+    body = JSON.parse response.body
+    assert_equal 1, body[0]['count']
+    assert_equal '2022-01-02T03:04:05.000Z', body[0]['last_played_at']
+  end
+
+  test 'should get stats and filter plays by album' do
+    create(:play, track: @track, user: @user)
+    create(:play, track: create(:track), user: @user)
+    get stats_plays_url(album_id: @track.album_id)
+    assert_response :success
+    body = JSON.parse response.body
+    assert_equal 1, body.length
+  end
+
+  test 'should get stats and filter plays by date' do
+    create(:play, track: @track, user: @user, played_at: 5.days.ago)
+    create(:play, track: @track, user: @user, played_at: 3.days.ago)
+    create(:play, track: @track, user: @user, played_at: DateTime.current)
+    create(:play, track: @track, user: @user)
+    get stats_plays_url(played_before: 2.days.ago, played_after: 4.days.ago)
+    assert_response :success
+    body = JSON.parse response.body
+    assert_equal 1, body[0]['count']
+  end
+
+  test 'should get stats and filter plays by artist' do
+    ta = create(:track_artist, track: @track)
+    create(:play, track: @track, user: @user)
+    create(:play, track: create(:track_artist).track, user: @user)
+    get stats_plays_url(artist_id: ta.artist_id)
+    assert_response :success
+    body = JSON.parse response.body
+    assert_equal 1, body.length
+  end
 end
