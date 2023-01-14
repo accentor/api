af8e7095a9b95dde38cf80b96a8cc790848ab6fb
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-01-14 13:40:54 +0100

    Also update version in flake.nix

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           default = accentor-api;
           accentor-api = pkgs.stdenv.mkDerivation rec {
             pname = "accentor-api";
-            version = "0.18.1";
+            version = "0.18.2";
 
             src = pkgs.lib.cleanSourceWith { filter = name: type: !(builtins.elem name [ ".github" "flake.lock" "flake.nix" ]); src = ./.; name = "source"; };
 
