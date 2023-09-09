f351c130a8a562ec41703aedf89a0f022f89cb3c
Robbe Van Petegem <git@robbevp.be>
2023-09-09 16:09:11 +0200

    Run cron jobs using GoodJob

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -7,5 +7,10 @@ Rails.application.configure do
   # GoodJob recurring jobs
   # See https://github.com/bensheldon/good_job#cron-style-repeatingrecurring-jobs
   config.good_job.enable_cron = true
-  config.good_job.cron = {}
+  config.good_job.cron = {
+    clean_transcode_cache: {
+      cron: 'every 4 hours',
+      class: 'TranscodeCacheCleanJob'
+    }
+  }
 end
