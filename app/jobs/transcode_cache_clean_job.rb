5ad55fee779dac1957ffbf3a86bbd6b490df00b6
Robbe Van Petegem <git@robbevp.be>
2023-09-17 08:45:47 +0200

    Fix typo

--- app/jobs/transcode_cache_clean_job.rb
+++ app/jobs/transcode_cache_clean_job.rb
@@ -1,5 +1,5 @@
 class TranscodeCacheCleanJob < ApplicationJob
-  queue_as :within_30_minuts
+  queue_as :within_30_minutes
 
   def perform
     TranscodedItem.where.not(last_used: (Rails.configuration.transcode_cache_expiry.call..)).destroy_all
