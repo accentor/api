368da2b770b7e422e50302295cff30fe08783a55
Robbe Van Petegem <robbe@vanpetegem.me>
2023-10-28 17:32:40 +0200

    Move recalculation of content lengths to background job (#498)
    * Move recalculation of content lengths to background job
    
    * Clean up tests
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Charlotte Van Petegem <charlotte@vanpetegem.me>

--- app/models/content_length.rb
+++ app/models/content_length.rb
@@ -14,16 +14,4 @@ class ContentLength < ApplicationRecord
 
   validates :audio_file, uniqueness: { scope: :codec_conversion }
   validates :length, presence: true
-
-  def self.destroy_all_and_recalculate
-    destroy_all
-
-    AudioFile.find_each do |af|
-      next unless Rails.application.config.recalculate_content_length_if.call af
-
-      CodecConversion.find_each do |cc|
-        CalculateContentLengthJob.set(queue: :whenever).perform_later(af, cc)
-      end
-    end
-  end
 end
