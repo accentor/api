19d8a43c852a9286e342ad49e7b424c51ec2d5bb
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-10-10 21:28:04 +0200

    Make audio_file#convert raise an exception on failure (#656)

--- test/models/audio_file_test.rb
+++ test/models/audio_file_test.rb
@@ -15,6 +15,15 @@
 require 'test_helper'
 
 class AudioFileTest < ActiveSupport::TestCase
+  class ProcessStub
+    def initialize(exit_status)
+      @exit_status = exit_status
+    end
+
+    def success? = @exit_status.zero?
+    def to_i = @exit_status
+  end
+
   def setup
     location = Location.create(path: Rails.root.join('test/files'))
     codec = Codec.create(extension: 'flac', mimetype: 'audio/flac')
@@ -26,10 +35,20 @@ class AudioFileTest < ActiveSupport::TestCase
 
     path = TranscodedItem.path_for(codec_conversion, SecureRandom.uuid)
 
-    Open3.stubs(:popen2).once.with do |*args, **kwargs|
+    Open3.stubs(:capture2).once.returns(['', ProcessStub.new(0)]).with do |*args, **kwargs|
       args.include?('ffmpeg') && args.include?(@audio_file.full_path) && args[-1] == path && kwargs.key?(:err)
     end
 
     @audio_file.convert(codec_conversion, path)
+  end
+
+  test 'convert should raise if underlying process fails' do
+    codec_conversion = create(:codec_conversion)
+
+    Open3.stubs(:capture2).once.returns(['', ProcessStub.new(9)])
+
+    assert_raises AudioFile::FailedTranscode do
+      @audio_file.convert(codec_conversion, TranscodedItem.path_for(codec_conversion, SecureRandom.uuid))
+    end
   end
 end
