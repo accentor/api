88da975792749bf9183be861ffda19d10da7834f
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-10-10 21:16:56 +0200

    Enqueue codec_conversion transcodes on slower queue

--- app/jobs/queue_transcoded_items_for_codec_conversion_job.rb
+++ app/jobs/queue_transcoded_items_for_codec_conversion_job.rb
@@ -6,7 +6,7 @@ class QueueTranscodedItemsForCodecConversionJob < ApplicationJob
   def perform(codec_conversion)
     AudioFile.find_in_batches do |batch|
       jobs = batch.filter_map do |af|
-        CreateTranscodedItemJob.new(af, codec_conversion) if Rails.configuration.queue_create_transcoded_item_if.call(af)
+        CreateTranscodedItemJob.new(af, codec_conversion).set(queue: :whenever) if Rails.configuration.queue_create_transcoded_item_if.call(af)
       end
       ActiveJob.perform_all_later(jobs)
     end
