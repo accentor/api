46e31e6017ad66e9e51c357473f426c25bf901cb
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-03-08 13:55:48 +0100

    Bump version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -4,7 +4,7 @@ module Accentor
   class Application
     module Version
       MAJOR = 0
-      MINOR = 22
+      MINOR = 23
       PATCH = 0
 
       STRING = [MAJOR, MINOR, PATCH].compact.join('.')
