78aad132171f9867982f6733e9a133b8eca6dab5
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-09-15 08:40:50 +0200

    Filter token from being logged (#813)

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,4 @@
 # Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
-Rails.application.config.filter_parameters += %i[passw secret]
+Rails.application.config.filter_parameters += %i[password token]
