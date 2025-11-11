4e3e83184958b9bd099f6ba73555e0cc18bc7124
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-11-11 11:54:33 +0100

    Touch track on audio file destroy

--- test/models/audio_file_test.rb
+++ test/models/audio_file_test.rb
@@ -90,4 +90,15 @@ class AudioFileTest < ActiveSupport::TestCase
       end
     end
   end
+
+  test "should touch the associated track when it's destroyed" do
+    one_day_ago = 1.day.ago
+    track = Track.create(number: 0, title: 'title', album: Album.create(title: 'title'), audio_file: @audio_file, updated_at: one_day_ago)
+
+    assert_equal one_day_ago, track.updated_at
+
+    @audio_file.destroy
+
+    assert_not_equal one_day_ago, track.reload.updated_at
+  end
 end
