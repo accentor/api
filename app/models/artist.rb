eea2d91924095032396b2bebfece39e817cce74b
Robbe Van Petegem <git@robbevp.be>
2026-08-15 12:17:10 +0200

    Use `update_all`

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -46,11 +46,9 @@ class Artist < ApplicationRecord
     errors.add(:track_artists, :tracks_overlap) if other.track_artists.map { |ta| [ta.track_id, ta.role] }.intersect?(track_artists.map { |ta| [ta.track_id, ta.role] })
     return false if errors.present?
 
-    other.album_artists.find_each do |aa|
-      # rubocop:disable Rails/SkipsModelValidations -- we skip validations, as this would check the separators while we're moving over the album artists
-      aa.update_columns(artist_id: id)
-      # rubocop:enable Rails/SkipsModelValidations
-    end
+    # rubocop:disable Rails/SkipsModelValidations -- we skip validations, as this would check the separators while we're moving over the album artists
+    other.album_artists.update_all(artist_id: id)
+    # rubocop:enable Rails/SkipsModelValidations
 
     other.track_artists.find_each do |ta|
       ta.update(artist_id: id)
